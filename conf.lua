79252fd4c415c1ce335c741f64242df327a5052c
Victor <victor47n@gmail.com>
2019-10-07 22:42:53 -0300

    Control ede colisão e jogo funcionando

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+  t.window.width = 480
+  t.window.height = 800
+  t.window.title = "Airplane Shooting"
+end
--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ width = nil
 height = nil
 
 -- Tabela de parâmetros do personagem
-player = {posx = 0, posy = 0, veloc = 150, img = nil}
+player = {posx = 0, posy = 0, veloc = 300, img = nil}
 
 -- Timers para sincronização
 shooting = true
@@ -15,7 +15,7 @@ timeShot = shotMax
 imgProj = nil
 
 -- Tabela de controle dos projéteis
-projectiles = nil
+projectiles = {}
 
 -- Temporização dos inimigos
 dtMaxCreateEnemy = 0.4
@@ -47,12 +47,12 @@ function love.load()
   player.posy = starty
   
   -- Cargas das outras imagens do ambiente
-  background = love.graphics.newImage('assets/fundo.png')
-  imgProj = love.graphics.newImage('assets/projetil.png')
-  imgEnemy = love.graphics.newImage('assets/inimigo.png')
+  background = love.graphics.newImage('assets/background.png')
+  imgProj = love.graphics.newImage('assets/projectile.png')
+  imgEnemy = love.graphics.newImage('assets/enemy.png')
   
   -- Carga do efeito sonoro do tiro
-  shot = love.audio.newSource('assets/tiro.wav', 'static')
+  shot = love.audio.newSource('assets/shoot.wav', 'static')
 end
 
 function love.update(dt)
@@ -86,9 +86,95 @@ function love.update(dt)
     timeShot = shotMax
   end
   
-  -- Continuamos aqui na proxima aula...
+  -- Atualizar a lista de projéteis
+  for i, proj in ipairs(projectiles) do
+    proj.y = proj.y - (250 * dt)
+    if proj.y < 0 then -- Remove o projétil que saiu da tela
+      table.remove(projectiles, i)
+    end
+  end
+  
+  -- Temporização da onda de inimigos
+  dtCurrentEnemy = dtCurrentEnemy - (1 * dt)
+  if dtCurrentEnemy < 0 then
+    dtCurrentEnemy = dtMaxCreateEnemy
+    -- Criando uma instância do inimigo
+    posDynamic = math.random(10, width - imgEnemy:getWidth())
+    lvlEnemy = { x = posDynamic, y = -10, img = imgEnemy }
+    table.insert(enemies, lvlEnemy)
+  end
+  
+  -- Atualizando posição dos inimigos
+  for i, enemy in ipairs(enemies) do
+    enemy.y = enemy.y + (200 * dt)
+    if enemy.y > 850 then -- remover se ultrapassar o final da tela
+      table.remove(enemies, i)
+    end
+  end
+  
+  -- Controlando as colisões
+  -- Tendo como base que devemos ter menos inimigos do que projéteis
+  -- Vamo processar os inimigos primeiros
+  for i, enemy in ipairs(enemies) do
+    for j, proj in ipairs(projectiles) do
+      if collision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(),
+        proj.x, proj.y, proj.img:getWidth(), proj.img:getHeight()) then
+        table.remove(projectiles, j)
+        table.remove(enemies, i)
+        score = score + 10
+      end
+    end
+    
+    -- Agora a colisão com meu personagem
+    if collision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(),
+      player.posx, player.posy, player.img:getWidth(), player.img:getHeight()) and alive then
+      table.remove(enemies, i)
+      alive = false
+    end
+  end
+  
+  -- Reiniciar o jogo se R for pressionado
+  if not alive and love.keyboard.isDown('r') then
+    -- Limpar a lista de inimigos e projéteis da tela
+    projectiles = {}
+    enemies = {}
+    
+    -- Reinicializa os temporizadores
+    timeShot = shotMax
+    dtCurrentEnemy = dtMaxCreateEnemy
+    
+    -- Movimenta o jogador para a posição inicial
+    player.posx = startx
+    player.posy = starty
+    
+    -- Reinicia o placar e vida
+    score = 0
+    alive = true
+  end
+  
 end
 
+function love.draw()
+  love.graphics.draw(background, 0, 0)
+  if alive then
+    love.graphics.draw(player.img, player.posx, player.posy)
+  else
+    love.graphics.print("Pressione R para reinicializar ou ESC para sair!",love.graphics.getWidth()/2 - 50,
+      love.graphics.getHeight()/2 - 10)
+  end
   
-      
+  -- Desenhar a lista de projéteis
+  for i, proj in ipairs(projectiles) do
+    love.graphics.draw(proj.img, proj.x, proj.y)
+  end
   
+  -- Desenhar a lista de inimigos
+  for i, enemy in ipairs(enemies) do
+    love.graphics.draw(enemy.img, enemy.x, enemy.y)
+  end
+  
+  -- Pontuação
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.print("PONTO: " .. tostring(score), 10, 10)
+  
+end
